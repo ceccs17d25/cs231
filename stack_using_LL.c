435ac76e1b7f43dd899e26be5b488180fee80a0d
ceccs17d25 <42906161+ceccs17d25@users.noreply.github.com>
2018-11-07 23:36:01 +0530

    Add files via upload

---
+++ stack_using_LL.c
@@ -0,0 +1,66 @@
+							//Name Haridev s
+							//program name: stack using LL
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void main()
+{
+    int cho;
+    struct node *temp,*top,*head;
+    head=(struct node*)malloc(sizeof(struct node));
+    head->data=0;
+    head->link=NULL;
+    top=head->link;
+    do
+    {
+        
+        printf("\nMENU\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\nEnter Choice: ");
+        scanf("%d",&cho);
+
+        if(cho==1)
+        {
+            temp=(struct node*)malloc(sizeof(struct node));
+            printf("enter the element: ");
+            scanf("%d",&temp->data);
+            temp->link=NULL;
+            head->link=temp;
+            top=head->link;
+        }
+        else if(cho==2)
+        {
+            if(top==NULL)
+            {
+                printf("stack is empty!!!");
+            }
+            else
+            {
+                printf("popped out: %d",top->data);
+                temp=top;
+                top=top->link;
+                head->link=top;
+                free(temp);
+            }
+        }
+
+        else if(cho==3)
+        {
+            printf("elements: ");
+            temp=top;
+            while(temp!=NULL)
+            {
+                printf("%d ",temp->data);
+                temp=temp->link;
+            }
+        }
+        
+    }while(cho==1 || cho==2 || cho==3);
+}
+    
+
