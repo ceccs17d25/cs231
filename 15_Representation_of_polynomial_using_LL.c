a2018ec30417eb7350546d6aca470a3fa9b0248f
ceccs17d25 <42906161+ceccs17d25@users.noreply.github.com>
2018-11-29 21:04:02 +0530

    Update 15_Representation_of_polynomial_using_LL.c

--- 15_Representation_of_polynomial_using_LL.c
+++ 15_Representation_of_polynomial_using_LL.c
@@ -61,3 +61,19 @@ void main()
         printf("0\n");
     }
 }
+OUTPUT:
+	Enter coefficient: 3
+Enter exponent: 2
+1. Add more elements
+2. Continue
+Enter choice: 1
+Enter coefficient: 4
+Enter exponent: 3
+1. Add more elements
+2. Continue
+Enter choice: 2
+1. Display polynomial
+2. Exit
+Enter choice: 1
+F(p) = 3 p^2 + 4 p^3 + 0
+
