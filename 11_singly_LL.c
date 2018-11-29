5535611c64b27e54b798ea7e26f88487d0f943a9
ceccs17d25 <42906161+ceccs17d25@users.noreply.github.com>
2018-11-29 20:57:17 +0530

    Update 11_singly_LL.c

--- 11_singly_LL.c
+++ 11_singly_LL.c
@@ -121,4 +121,179 @@ void main()
         	}
     }while(cho==1 || cho==2 || cho==3 || cho==4 || cho==5);
 }
+	OUTPUT:
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:1
+	Enter data: 3
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:1
+	Enter data: 4
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	4 -> 3 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:2
+	Enter data: 1
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	4 -> 3 -> 1 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:3
+	Enter key: 3
+	Enter data: 2
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	4 -> 3 -> 2 -> 1 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:4
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	3 -> 2 -> 1 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:5
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	3 -> 2 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:6
+	Enter key: 2
+
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:8
+	3 -> NULL
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:7
+	Enter key: 3
+
+	Element found at 1
+	MENU
+	1.insertion(begin)
+	2.Insertion(end)
+	3.Insertion(after key)
+	4.Deletion(front)
+	5.Deletion(end)
+	6.Deletion(key)
+	7.Search
+	8.Display
+	Enter choice:9
+
 
